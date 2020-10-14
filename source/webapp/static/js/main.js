d0bad8790230853bc31b17a8842a0b553d4af910
ilia <jamiashvili@gmail.com>
2020-10-14 20:23:31 +0600

    Добавлена возможность делать AJAX-запросы с данными. Добавлен запрос csrf-токена

--- source/webapp/static/js/main.js
+++ source/webapp/static/js/main.js
@@ -1,5 +1,39 @@
-async function makeRequest(url, method='GET') {
-    let response = await fetch(url, {method});
+const BASE_URL = 'http://localhost:8000/api/v1/';
+
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+async function makeRequest(url, method='GET', data=undefined) {
+    let opts = {method, headers: {}};
+
+    if (!csrfSafeMethod(method))
+        opts.headers['X-CSRFToken'] = getCookie('csrftoken');
+
+    if (data) {
+        opts.headers['Content-Type'] = 'application/json';
+        opts.body = JSON.stringify(data);
+    }
+
+    let response = await fetch(url, opts);
 
     if (response.ok) {  // нормальный ответ
         return await response.text();
@@ -16,7 +50,7 @@ async function onLike(event) {
     let url = likeBtn.href;
 
     try {
-        let response = await makeRequest(url);
+        let response = await makeRequest(url, 'POST');
         const counter = likeBtn.parentElement.getElementsByClassName('counter')[0];
         counter.innerText = response;
     }
@@ -29,13 +63,14 @@ async function onLike(event) {
     unlikeBtn.classList.remove('hidden');
 }
 
+
 async function onUnlike(event) {
     event.preventDefault();
     let unLikeBtn = event.target;
     let url = unLikeBtn.href;
 
     try {
-        let response = await makeRequest(url);
+        let response = await makeRequest(url, 'DELETE');
         console.log(response);
         const counter = unLikeBtn.parentElement
             .getElementsByClassName('counter')[0];
